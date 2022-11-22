9754d0ea07c7bbeaa5777ff22dfdc2d9bc8189cd
Ester Cortes <002escor@gmail.com>
2022-11-22 02:15:52 +0100

    implementing some sort of scoreboard+storing timer

--- script/script.js
+++ script/script.js
@@ -1,10 +1,30 @@
 // stopwatch
-const startButton = document.getElementById(""); //link to whatever is in the HTML
-const stopButton = document.getElementById(""); //link to whatever is in the HTML
-const stopwatch = document.getElementById(""); //link to whatever is in the HTML
+const startButton = document.getElementById("startGameButton"); //link to whatever is in the HTML
+const stopButton = document.getElementById("stopButton"); //link to whatever is in the HTML
+const stopwatch = document.getElementById("timer"); //link to whatever is in the HTML
+const scoreboard = document.getElementById("scoreboard");
 
 let timerStart;
 let timer;
+
+let scoreArray = JSON.parse(localStorage.getItem("scoreArray")) || [];
+
+const setScoreboard = () => {
+  let i = 0;
+  scoreArray.forEach(() => {
+    console.log(localStorage.getItem("scoreArray"));
+    console.log(JSON.parse(localStorage.getItem("scoreArray")));
+    scoreboardElement = document.createElement("li");
+    scoreboardElement.innerHTML =
+      JSON.parse(localStorage.getItem("scoreArray"))[i].player +
+      ": " +
+      JSON.parse(localStorage.getItem("scoreArray"))[i].score;
+    scoreboard.append(scoreboardElement);
+    console.log(scoreboardElement);
+    i++;
+  });
+};
+setScoreboard();
 
 const updateStopwatch = () => {
   const time = Date.now() - timerStart;
@@ -26,7 +46,18 @@ startButton.onclick = () => {
   }, 10);
 };
 
+const storeScore = (currentScore) => {
+  let name = "Ester";
+  let playerScore = { player: name, score: currentScore };
+  scoreArray.push(playerScore);
+  localStorage.setItem("scoreArray", JSON.stringify(scoreArray));
+  console.log(scoreArray);
+  console.log(JSON.parse(localStorage.getItem("scoreArray")));
+  return scoreArray;
+};
+
 stopButton.onclick = () => {
+  storeScore(stopwatch.innerText);
   clearInterval(timer);
 };
 
