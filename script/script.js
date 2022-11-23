b70bb9af663618ccfe7310a28b6413770674e1c5
Francisco Sabino <fdasabino@gmail.com>
2022-11-23 11:15:31 +0100

    fixed some naming coventions

--- script/script.js
+++ script/script.js
@@ -2,9 +2,14 @@ const apiURL = "https://pokeapi.co/api/v2/pokemon/";
 
 // HTML Elements
 const startButton = document.getElementById("startGameButton"); //link to whatever is in the HTML
-const scoreboard = document.getElementById("scoreboard");
 const gameBoard = document.querySelector("game_board");
+
+// loader
 const loader = document.getElementById("loader");
+
+//Scoreboard
+const scoreboardWrapper = document.getElementById("scoreboard");
+let scoreboardElement = document.createElement("li");
 
 let scoreArray = JSON.parse(localStorage.getItem("scoreArray")) || [];
 
@@ -42,12 +47,12 @@ const setScoreboard = () => {
   scoreArray.forEach(() => {
     console.log(localStorage.getItem("scoreArray"));
     console.log(JSON.parse(localStorage.getItem("scoreArray")));
-    scoreboardElement = document.createElement("li");
+
     scoreboardElement.innerHTML =
       JSON.parse(localStorage.getItem("scoreArray"))[i].player +
       ": " +
       JSON.parse(localStorage.getItem("scoreArray"))[i].score;
-    scoreboard.append(scoreboardElement);
+    scoreboardWrapper.append(scoreboardElement);
     console.log(scoreboardElement);
     i++;
   });
